0b82dd4262210ead1dee048e247890d49da1515f
Amz <2017159@brunel.ac.uk>
2024-04-05 15:17:52 +0100

    Added New Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,6 +14,7 @@ import BTM3 from "../css/images/BTM3.png";
 import JHC1 from "../css/images/JHC1.png";
 import JHC2 from "../css/images/JHC2.png";
 import JHC3 from "../css/images/JHC3.png";
+import JHC4 from "../css/images/JHC4.png";
 
 import Healio1 from "../css/images/Healio1.png";
 import Healio2 from "../css/images/Healio2.png";
@@ -22,6 +23,13 @@ import Healio4 from "../css/images/Healio4.png";
 import Healio5 from "../css/images/Healio5.png";
 import Healio6 from "../css/images/Healio6.png";
 import Healio7 from "../css/images/Healio7.png";
+
+import HecoCost1 from "../css/images/HecoCost1.png";
+import HecoCost2 from "../css/images/HecoCost2.png";
+import HecoCost3 from "../css/images/HecoCost3.png";
+import HecoCost4 from "../css/images/HecoCost4.png";
+import HecoCost5 from "../css/images/HecoCost5.png";
+import HecoCost6 from "../css/images/HecoCost6.png";
 
 function Projects() {
   return (
@@ -68,6 +76,33 @@ function Projects() {
           className="mySwiper"
         >
           <SwiperSlide>
+            <img src={HecoCost1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={HecoCost2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={HecoCost3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={HecoCost4} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={HecoCost5} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={HecoCost6} alt="" />
+          </SwiperSlide>
+        </Swiper>
+
+        <div className="link">HecoCost</div>
+
+        <Swiper
+          navigation={true}
+          modules={[Navigation, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
             <img src={BTM1} alt="" />
           </SwiperSlide>
           <SwiperSlide>
@@ -95,6 +130,9 @@ function Projects() {
           </SwiperSlide>
           <SwiperSlide>
             <img src={JHC3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={JHC4} alt="" />
           </SwiperSlide>
         </Swiper>
 
