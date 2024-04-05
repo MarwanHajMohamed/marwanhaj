01426aad70a9c2628403ba82730e36fa68a75d2c
Amz <2017159@brunel.ac.uk>
2024-04-05 13:45:49 +0100

    Edited About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,56 @@
-import React from 'react'
-import '../css/about.css'
-import Card from './commonStructures/Card'
+import React from "react";
+import "../css/about.css";
+import Card from "./commonStructures/Card";
 
 export default function About() {
-    return (
-      <div>
-        <div className="about-container" id='about'>
-          <div className="about-title">About Me</div>
-          <div className="description">
-            I am a passionate, quick learning programmer that 
-            constantly broadens my knowledge in different languages to earn more experience 
-            in this field. I have knowledge in the following so far:
-          </div>
-          <div className="card-container">
-            <Card 
-              icon="fa-brands fa-react"
-              title="ReactJS Development"
-              text="During the summer of first year university, I took on the challenge of learning ReactJS,
-              and have since worked with a company to develop the front-end of their website."
-            />
-            <Card 
-              title="CSS"
-              icon="fa-brands fa-css3-alt"
-              text="Whilst practicing ReactJS development, I have picked up valuable skills within CSS.
-              These skills helped me style components to be visually appealing and easily understandable."
-            />
-            <Card 
-              icon="fa-brands fa-java"
-              title="Java Development"
-              text="As a Computer Science student, working with Java has made it possible to create 
-              applications such as an Airport booking system, a Notes application and so on."
-            />
-          </div>
+  return (
+    <div>
+      <div className="about-container" id="about">
+        <div className="about-title">About Me</div>
+        <div className="description">
+          I am a passionate, quick learning programmer that constantly broadens
+          my knowledge in different languages to earn more experience in this
+          field. I have knowledge in the following so far:
+        </div>
+        <div className="card-container">
+          <Card
+            icon="fa-brands fa-python"
+            title="Python"
+            text="Proficient in Python development with extensive experience in designing, developing and testing 
+            different NLP algorithms."
+          />
+          <Card
+            icon="fa-solid fa-computer"
+            title="Machine Learning"
+            text="Experience in ML algorithms such as Random Forest Classifier, applying it to the Healio chatbot
+            to understand symptoms and diagnose users."
+          />
+          <Card
+            icon="fa-brands fa-react"
+            title="ReactJS"
+            text="Skilled ReactJS developer capable in building dynamic and responsive user interfaces, gaining
+            knowledge through building various websites."
+          />
+          <Card
+            icon="fa-solid fa-mobile-screen"
+            title="React Native"
+            text="Skilled React Native developer utilising my knowledge in React to build cross platform mobile
+            applications such as Al Madani (Shown in Projects)."
+          />
+          <Card
+            icon="fa-solid fa-power-off"
+            title="SpringBoot"
+            text="Expert SpringBoot developer, building scalable Java-based web applications while
+            creating Rest-APIs integrated with SQL databases."
+          />
+          <Card
+            icon="fa-brands fa-java"
+            title="Java"
+            text="Experience in Java development, leveraging its robust features and libraries to develop versatile
+            applications such as an Airport booking system."
+          />
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
